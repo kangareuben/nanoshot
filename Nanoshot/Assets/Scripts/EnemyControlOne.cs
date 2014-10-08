2ccc7b6f374160e36d10cc05924507d2508d8441
rmb1952 <rmb1952@rit.edu>
2014-10-08 17:36:41 -0400

    Added EnemyControlOne

---
+++ Nanoshot/Assets/Scripts/EnemyControlOne.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyControlOne : MonoBehaviour {
+
+	public float speed;
+
+	private Object _enemyOne;
+
+	// Use this for initialization
+	void Start () {
+		_enemyOne = Resources.Load("Prefabs/EnemyOne");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float velY = 0.0f;
+		velY += Mathf.Sin(Time.time * 3f) * 6;
+		rigidbody2D.velocity = new Vector3(-speed, velY, 0);
+	}
+
+	void OnTriggerEnter2D(Collider2D e)
+	{
+		if(e.gameObject.tag == "LeftWall")
+		{
+			Collider.Destroy(this.gameObject);
+		}
+	}
+
+	void OnDestroy()
+	{
+		Instantiate (_enemyOne, new Vector3(12f, Random.Range(-2, 1), 0), transform.rotation);
+	}
+}
