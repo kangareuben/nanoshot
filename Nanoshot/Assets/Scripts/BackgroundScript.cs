883c12d5dd75114d146b61408a6a202d6e679759
Steven S. <sas8463@rit.edu>
2014-10-11 19:39:07 -0400

    Background Scrolling
    ^

---
+++ Nanoshot/Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScript : MonoBehaviour {
+	private float x;
+
+	// Use this for initialization
+	void Start () {
+		x = this.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x -= .05f;
+		if (x < -24){
+			x = 24;
+		}
+		Vector3 temp = this.transform.position;
+		temp.x = x;
+		this.transform.position = temp;
+	}
+}
